7b8ea742549140ec7a6a99c0205a654ccbc6dc1a
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-09-06 17:42:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ db = SQLAlchemy(app)
 
 # Mail
 app.config.update(
-    MAIL_SERVER=os.environ.get("MAIL_SERVER", "smtp.example.com"),
+    MAIL_SERVER=os.environ.get("MAIL_SERVER", "smtp.gmail.com"),
     MAIL_PORT=int(os.environ.get("MAIL_PORT", 587)),
     MAIL_USE_TLS=True,
     MAIL_USERNAME=os.environ.get("MAIL_USERNAME"),
     MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD"),
-    MAIL_DEFAULT_SENDER=os.environ.get("MAIL_DEFAULT_SENDER"),
+    MAIL_DEFAULT_SENDER=os.environ.get("gustinosspa@gmail.com"),
 )
 mail = Mail(app)
 
