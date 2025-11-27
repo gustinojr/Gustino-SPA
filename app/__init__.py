9187c1089b8fda0114fa01e9006867d7350a1a58
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-27 23:03:25 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,26 +6,24 @@ import os
 db = SQLAlchemy()
 load_dotenv()
 
-def create_app():
+    def create_app():
     app = Flask(__name__)
-    app.config.from_object("config.Config")
+    app.config.from_object(Config)
 
     db.init_app(app)
 
-    # Register blueprints
+    # registra i blueprint
     from app.routes.home import home_bp
-    from app.routes.bot import bot_bp
-    from app.routes.booking import booking_bp
     from app.routes.register import register_bp
-    from app.telegram_polling import start_polling
-    
+    from app.routes.bot import bot_bp
     app.register_blueprint(home_bp)
-    app.register_blueprint(bot_bp)
-    app.register_blueprint(booking_bp)
     app.register_blueprint(register_bp)
+    app.register_blueprint(bot_bp)
 
-
+    # ⚠️ AVVIA IL POLLING SOLO ORA
     with app.app_context():
-        db.create_all()
-    start_polling(app)
+        from app.telegram_polling import start_polling
+        start_polling(app)
+
     return app
+
