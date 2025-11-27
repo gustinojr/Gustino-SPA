fd13dcfc5f1eb68e5e5a2773816281859fd1a4f1
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-27 22:47:49 +0100

    Update home.py

--- app/routes/home.py
+++ app/routes/home.py
@@ -7,3 +7,7 @@ home_bp = Blueprint("home_bp", __name__)
 @home_bp.route("/")
 def home():
     return render_template("home.html")
+
+@home_bp.route("/startBot")
+def start_bot():
+    return render_template("start_bot.html")
