179a1ce8033b7bb55fee1555d54d8336a9f65110
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-27 21:39:07 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,16 +14,15 @@ def create_app():
 
     # Register blueprints
     from app.routes.home import home_bp
-    from app.routes.register import register_bp
-    from app.routes.booking import booking_bp
-    from app.routes.telegram_webhook import telegram_bp
     from app.routes.bot import bot_bp
-    
-    app.register_blueprint(bot_bp)
+    from app.routes.booking import booking_bp
+    from app.routes.register import register_bp
+
     app.register_blueprint(home_bp)
-    app.register_blueprint(register_bp)
+    app.register_blueprint(bot_bp)
     app.register_blueprint(booking_bp)
-    app.register_blueprint(telegram_bp)
+    app.register_blueprint(register_bp)
+
 
     with app.app_context():
         db.create_all()
