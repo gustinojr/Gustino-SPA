f7481435d9a5877a162514cec418c73d1652e408
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-27 23:07:18 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
+from config import Config
 
 db = SQLAlchemy()
-load_dotenv()
 
-    def create_app():
+def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
 
-    # registra i blueprint
+    # BLUEPRINTS
     from app.routes.home import home_bp
     from app.routes.register import register_bp
     from app.routes.bot import bot_bp
+
     app.register_blueprint(home_bp)
     app.register_blueprint(register_bp)
     app.register_blueprint(bot_bp)
 
-    # ⚠️ AVVIA IL POLLING SOLO ORA
+    # AVVIO POLLING TELEGRAM
     with app.app_context():
         from app.telegram_polling import start_polling
         start_polling(app)
 
     return app
-
