72ba5760d8a849f822c9d42d2b5e7b083c9b44ca
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-28 17:39:06 +0100

    Update bot.py

--- app/routes/bot.py
+++ app/routes/bot.py
@@ -3,6 +3,8 @@ from app.telegram_polling import start_bot_polling
 
 bot_bp = Blueprint("'bot'", __name__)
 
+bot_running = False
+
 @bot_bp.route('/start-bot')
 def start_bot():
     start_bot_polling()
