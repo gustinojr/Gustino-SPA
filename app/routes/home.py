ecfd268e6795799707dac2dfd48a9c49c242e828
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-28 17:27:38 +0100

    Update home.py

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from app.telegram_polling import start_bot_polling
 
 home_bp = Blueprint("home_bp", __name__)
 
-@home_bp.route("/wait-for-chatid")
-def wait_for_chatid():
-        # Avvia polling (se non è già in esecuzione)
-    start_bot_polling()
-    return render_template("wait_for_chatid.html")
-
-
 @home_bp.route("/")
 def home():
     return render_template("home.html")
+
+@home_bp.route("/start-bot")
+def start_bot():
+    start_bot_polling()
+    return redirect(url_for("home_bp.wait_for_chatid"))
+
+@home_bp.route("/wait-for-chatid")
+def wait_for_chatid():
+    return render_template("wait_for_chatid.html")
