c49a7e216f6be4f878ee096c0c400d93fa75936b
Raffaele Antonio D'Agostino <102957829+gustinojr@users.noreply.github.com>
2025-11-29 15:06:54 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
-# app/__init__.py
 from flask import Flask
-from .bot import bp as bot_bp
+from app.routes.home import home_bp
+from app.routes.register import register_bp
+from app.routes.bot import bot_bp
+import os
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.getenv("SECRET_KEY", "supersecretkey")
+
+    # Registrazione Blueprint
+    app.register_blueprint(home_bp)
+    app.register_blueprint(register_bp)
     app.register_blueprint(bot_bp)
+
+
     return app
